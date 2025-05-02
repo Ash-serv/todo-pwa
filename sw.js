cadc15c4ca19cacd0a1b9087cf371e27779cad65
Ash-serv <ashleyvandermuller@gmail.com>
2025-05-02 03:26:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,29 @@
-const CACHE_NAME = 'todo-pwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  'https://cdn.tailwindcss.com'
-];
-
+const CACHE_NAME = 'todo-pwa-v2'; // Change version when updating
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192x192.png'
+      ]);
+    })
   );
 });
-
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
