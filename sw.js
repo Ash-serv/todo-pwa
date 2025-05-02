5b4d0de86df916513f61a1de4339e59afbd0537b
Ash-serv <ashleyvandermuller@gmail.com>
2025-05-02 05:02:53 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,8 @@
+
+
 const CACHE_NAME = 'todo-pwa-v4'; // Update version
+
+// Install event: Cache essential files and activate immediately
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
@@ -11,16 +15,21 @@ self.addEventListener('install', event => {
       ]);
     })
   );
+  self.skipWaiting(); // Activate new service worker immediately
 });
+
+// Activate event: Clear old caches and take control of clients
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
       );
-    })
+    }).then(() => self.clients.claim()) // Take control of all open clients
   );
 });
+
+// Fetch event: Serve from cache, fallback to network
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => {
@@ -28,4 +37,3 @@ self.addEventListener('fetch', event => {
     })
   );
 });
-
